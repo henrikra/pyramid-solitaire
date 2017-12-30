7e0c443f76b78b71fbb759d31f9afdfefa12aec9
Henrik Raitasola <henrik.raitasola@gmail.com>
2017-12-30 18:29:06 +0200

    Render cards to ui

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,12 @@ import * as R from 'ramda';
 
 const logo = require('./logo.svg');
 
-// type Card = {
-//   image: string;
-//   value: number;
-//   isSelectable: boolean;
-//   isDeleted: boolean;
-// };
+type Card = {
+  image: string;
+  value: number;
+  isSelectable: boolean;
+  isDeleted: boolean;
+};
 
 // const lol: Card = {
 //   image: "https://deckofcardsapi.com/static/img/KH.png",
@@ -19,18 +19,6 @@ const logo = require('./logo.svg');
 //   isDeleted: true
 // };
 
-axios
-  .get('https://deckofcardsapi.com/api/deck/new/draw/?count=28')
-  .then(response => {
-    const { cards } = response.data;
-    const pyramidCards = [0, 1, 3, 6, 10, 15, 21].map((startingIndex, index) =>
-      R.slice(startingIndex, startingIndex + index + 1, cards)
-    );
-    1;
-  })
-  .catch(error => {
-    console.log('error', error);
-  });
 /*
 
 kortti on valittavissa jos
@@ -62,7 +50,31 @@ oleva kortti ja cardIndex + 1 ovat poissa
 ]
 */
 
+interface State {
+  pyramidCards: Array<Array<Card>>;
+}
+
 class App extends React.Component {
+  state: State = {
+    pyramidCards: []
+  };
+
+  componentWillMount() {
+    axios
+      .get('https://deckofcardsapi.com/api/deck/new/draw/?count=28')
+      .then(response => {
+        const { cards } = response.data;
+        const pyramidCards = [0, 1, 3, 6, 10, 15, 21].map(
+          (startingIndex, index) =>
+            R.slice(startingIndex, startingIndex + index + 1, cards)
+        );
+        this.setState({ pyramidCards });
+      })
+      .catch(error => {
+        console.log('error', error);
+      });
+  }
+
   render() {
     return (
       <div className="App">
@@ -70,9 +82,11 @@ class App extends React.Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        {this.state.pyramidCards.map(pyramidCardRow => (
+          <div>
+            {pyramidCardRow.map(pyramidCard => <img src={pyramidCard.image} />)}
+          </div>
+        ))}
       </div>
     );
   }
